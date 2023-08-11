14f6b5f35f99ebca3d34d03cca502d9c4824e6f0
Volodymyr2610 <129291672+Volodymyr2610@users.noreply.github.com>
2023-08-11 20:28:01 +0300

    1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,4 +1,21 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-const iframe = document.querySelector('iframe')
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const STORAGE_KEY = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(saveTimePlayer, 1000));
+
+function saveTimePlayer({ seconds }) {
+  localStorage.setItem(STORAGE_KEY, seconds);
+}
+
+function updateTimePlayer() {
+  if (localStorage.getItem(STORAGE_KEY)) {
+    player.setCurrentTime(localStorage.getItem(STORAGE_KEY));
+  }
+}
+
+updateTimePlayer();
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+import throttle from 'lodash.throttle';
 
+
+const STORAGE_KEY = 'feedback-form-state';
+
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    input: document.querySelector('.feedback-form input'),
+    textarea: document.querySelector('.feedback-form textarea'),
+  };
+  
+  handleLocalStorage();
+  
+  refs.form.addEventListener('submit', handleSubmit);
+  refs.form.addEventListener('input', throttle(handleInput, 500));
+  
+  function handleInput() {
+    const formData = {
+      email: refs.input.value,
+      message: refs.textarea.value,
+    };
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+  }
+  
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (localStorage.getItem(STORAGE_KEY)) {
+      const data = JSON.parse(localStorage.getItem(STORAGE_KEY));
+      console.log(data);
+    }
+    localStorage.removeItem(STORAGE_KEY);
+    refs.form.reset();
+  }
+  
+  function handleLocalStorage() {
+    const defaultvalue = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    if (!defaultvalue) {
+      return;
+    }
+    refs.input.value = defaultvalue.email || '';
+    refs.textarea.value = defaultvalue.message || '';
+  }
