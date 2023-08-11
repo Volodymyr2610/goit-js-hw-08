038a1e75866f9463227e1334d4275d7deda4a8f1
Volodymyr2610 <129291672+Volodymyr2610@users.noreply.github.com>
2023-08-11 20:37:17 +0300

    up

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,42 +1,59 @@
 import throttle from 'lodash.throttle';
 
 
-const STORAGE_KEY = 'feedback-form-state';
+const feedbackForm = document.querySelector('.feedback-form');
 
-const refs = {
-    form: document.querySelector('.feedback-form'),
-    input: document.querySelector('.feedback-form input'),
-    textarea: document.querySelector('.feedback-form textarea'),
-  };
-  
-  handleLocalStorage();
-  
-  refs.form.addEventListener('submit', handleSubmit);
-  refs.form.addEventListener('input', throttle(handleInput, 500));
-  
-  function handleInput() {
-    const formData = {
-      email: refs.input.value,
-      message: refs.textarea.value,
-    };
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-  }
-  
-  function handleSubmit(event) {
-    event.preventDefault();
-    if (localStorage.getItem(STORAGE_KEY)) {
-      const data = JSON.parse(localStorage.getItem(STORAGE_KEY));
-      console.log(data);
+feedbackForm.addEventListener('input', throttle((collectData), 500));
+feedbackForm.addEventListener('submit', submitFeedback)
+
+let savedUserFeedback = localStorage.getItem("feedback-form-state")
+autoFillText();
+let feedbackData = {};
+
+if(savedUserFeedback){
+feedbackData = JSON.parse(savedUserFeedback)
+}
+
+
+function collectData(e){
+    if(e.target.value){
+    feedbackData[e.target.name] = e.target.value;
+    const feedbackDataJSON = JSON.stringify(feedbackData);
+    localStorage.setItem("feedback-form-state", feedbackDataJSON)
+}
+}
+    
+function autoFillText(){
+    savedUserFeedback = localStorage.getItem("feedback-form-state")
+    if(savedUserFeedback) {
+    const userData = JSON.parse(savedUserFeedback)
+    emailInputFill(userData);
+    messageInputFill(userData)
     }
-    localStorage.removeItem(STORAGE_KEY);
-    refs.form.reset();
-  }
-  
-  function handleLocalStorage() {
-    const defaultvalue = JSON.parse(localStorage.getItem(STORAGE_KEY));
-    if (!defaultvalue) {
-      return;
+}
+
+function emailInputFill (userData){
+    if(userData.email){
+       
+        feedbackForm.email.value = userData.email;
     }
-    refs.input.value = defaultvalue.email || '';
-    refs.textarea.value = defaultvalue.message || '';
-  }
+}
+
+function messageInputFill (userData){
+    if(userData.message){
+        feedbackForm.message.value = userData.message;
+    }
+}
+
+function submitFeedback(e){
+    e.preventDefault();
+    if(feedbackData.email && feedbackData.message) {
+    e.target.reset();
+    localStorage.removeItem("feedback-form-state")
+    console.log(feedbackData)
+    feedbackData = {};
+    return;
+}
+alert('Усі поля мають бути заповнені')
+return;
+}
